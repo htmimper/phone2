c79e94b7a1509103fce10e9d1d68be2c3cc986f5
htmimper <htmimper@gmail.com>
2025-05-20 21:59:52 +0900

    20250520
    05_オブジェクト指向(実装)
    演習③まで完了。

---
+++ Iphone2/src/Mp3Player.java
@@ -0,0 +1,9 @@
+
+public interface Mp3Player {// interface：イメージとしてはこれからルールを作りますよという宣言。
+	 public abstract void play();
+	 public abstract void stop();
+	 public abstract void next();
+	 public abstract void back();
+	//interfaceとabstract はセットで使う。
+}
+
